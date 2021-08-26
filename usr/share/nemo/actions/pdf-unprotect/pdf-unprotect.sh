81b9b4d096c67666c9fa8002d2b90e5008c6cf53
Soul Inferno DE <SoulInfernoDE@users.noreply.github.com>
2021-08-27 00:18:34 +0200

    Add files via upload
    initial commit of the main script files

---
+++ usr/share/nemo/actions/pdf-unprotect/pdf-unprotect.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#################################
+# PDF Unprotect by SoulInferno  #
+#################################
+# WIP/Unfinished/Not working yet#
+#################################
+#Base script (c)by Alfcx (THX!) #
+#################################
+#
+dir=`dirname "$1"` ;
+base=`basename "$1"` ;
+#
+# Get settings from config.ini
+scriptdir=`dirname "$0"` ;
+source "$scriptdir/config.ini" ;
+#
+# Set your language (Make sure it is supported, if not write a translation first manually)
+lang="de" ;
+#
+# If there's no translation for your language, import the english one.
+langdir="$scriptdir/lang" ;
+if [ -f "$langdir/$lang.ini" ] 
+   then source "$langdir/$lang.ini" ;
+   else source "$langdir/de.ini" ;
+fi ;
+#
+# Ask for the Name of the output file
+if ! ASKPWD=$(zenity --entry \
+  --title="$output_title" \
+  --text="$output_text" \
+  --width=300) ; then
+  exit;
+fi ;
+# 
+# Unprotect the file
+#pdftk '$dir/$base' input_pw $ASKPWD output cat output '$dir/$base''-unprotected.pdf';
+#
+# Example of what the finished line should do:
+#pdftk '$PATH_TO_FILE/$FILENAME.pdf' input_pw $ASKPWD output '$PATH_TO_FILE/$FILENAME''-unprotected.pdf';
+#
+# Delete input files (only if set to true in config.ini)
+if [[ "$delinput" = "true" ]] 
+   then rm $inputfiles ;
+fi ;
